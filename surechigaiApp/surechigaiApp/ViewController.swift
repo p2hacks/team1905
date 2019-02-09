926cc825dc1660d6f75c7f0c5c5ff09ee9ab8314
k-shuya <kwamua@me.com>
2019-02-09 23:39:04 +0900

    プロジェクトの追加

---
+++ surechigaiApp/surechigaiApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  surechigaiApp
+//
+//  Created by 川村周也 on 2019/02/09.
+//  Copyright © 2019 川村周也. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
