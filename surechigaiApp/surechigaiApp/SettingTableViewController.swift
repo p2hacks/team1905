2ff80abc9f172893839e236cdad78f4fc2503547
前田陸 <MdRk@github.com>
2019-02-11 20:53:15 +0900

    設定画面のレイアウト作成

---
+++ surechigaiApp/surechigaiApp/SettingTableViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SettingTableViewController.swift
+//  surechigaiApp
+//
+//  Created by 前田陸 on 2019/02/11.
+//  Copyright © 2019 川村周也. All rights reserved.
+//
+
+import UIKit
+
+class SettingTableViewController: UITableViewController {
+
+    @IBOutlet var settingTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
