8b5141b4aa5dc60a84239be5d845d31a24da1542
前田陸 <MdRk@github.com>
2019-02-16 15:18:56 +0900

    通知後tableViewを更新

--- surechigaiApp/surechigaiApp/NoticeListViewController.swift
+++ surechigaiApp/surechigaiApp/NoticeListViewController.swift
@@ -10,15 +10,43 @@ import UIKit
 
 class NoticeListViewController: UIViewController {
 
+    var noticeListCell = [""]
+    
     @IBOutlet weak var noticeListTableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        let appDelegate:AppDelegate = UIApplication.shared.delegate as! AppDelegate
+        appDelegate.noticeListCell = self
+        
+        noticeListTableView.delegate = self
+        noticeListTableView.dataSource = self
     }
     
+    func addCell() {
+        noticeListCell.insert("新規の広告を受信しました。", at: 0)
+        noticeListTableView.reloadData()
+    }
     
     @IBAction func pushBackBtn(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
     }
     
+}
+
+extension NoticeListViewController: UITableViewDelegate {
+    
+}
+
+extension NoticeListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return noticeListCell.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "noticeCell", for: indexPath)
+        cell.textLabel!.text = noticeListCell[indexPath.row]
+        return cell
+    }
 }
